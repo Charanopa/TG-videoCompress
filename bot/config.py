9a255c77c9f216c06a0675a78d17e828e67fb660
Charanopa <129950550+Charanopa@users.noreply.github.com>
2023-05-21 15:32:10 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,13 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("12001153", cast=int)
+    API_HASH = config("c388da32bc40c14a9d4dc64f8f96f134")
+    BOT_TOKEN = config("6278628468:AAGp4FYKM1V73kTbxnU0julBfAxhuVcDiZY")
     DEV = 1287276743
-    OWNER = config("OWNER")
+    OWNER = config("6113562037")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMB = config("https://telegra.ph/file/e63c07cd04a2255f7c185.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
