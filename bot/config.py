5b09580ee319eba65f664a719675260f0d577e45
Charanopa <129950550+Charanopa@users.noreply.github.com>
2023-05-21 15:32:55 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ try:
     BOT_TOKEN = config("6278628468:AAGp4FYKM1V73kTbxnU0julBfAxhuVcDiZY")
     DEV = 1287276743
     OWNER = config("6113562037")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
+    ffmpegcode = [" -c:v libx265 -preset medium -x265-params crf=28 -c:a aac -strict experimental -b:a 128k -map 0:v -map 0:a -map 0:s -c:a copy -c:s copy  -metadata 'title=Never_xy'"]
     THUMB = config("https://telegra.ph/file/e63c07cd04a2255f7c185.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
